438a01e880050012288bbf2e329d1746aaf08a5c
Ruslan <151436624+OneRite@users.noreply.github.com>
2024-02-25 16:33:43 +0200

    Delete laba_1 directory

--- laba_1/task1.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Введите число "))
-b = int(input("ВВедите число "))
-c = int(input("Введите число "))
-if a==b==c:
-    print(3)
-elif a==b or a==c or b==c or b==a:
-    print(2)
-else:
-    print(0)
--- laba_1/task2_2.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Введите кол-во ступенек "))
-for i in range (1, n+1):
-    for j in range (n-i):
-        print(" ", end="") 
-    for k in range (i,0,-1):
-        print(k , end="")
-    for l in range (2, i+1):
-        print(l ,end="")
-    print()
